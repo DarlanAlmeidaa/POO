69512d9c33f1a48129f4439e332819453e147720
DarlanAlmeida <164550484+DarlanAlmeidaa@users.noreply.github.com>
2025-05-12 19:06:36 -0300

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,11 @@
+public class Animal {
+    protected String nome;
+
+    public Animal(String nome) {
+        this.nome = nome;
+    }
+
+    public void falar() {
+        System.out.println("O animal faz um som");
+    }
+}
